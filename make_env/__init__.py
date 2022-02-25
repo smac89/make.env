4e99d85769a288f6f31d41a7dcc1135d32bb1655
smac89 <nobleman.code@gmx.com>
2022-02-25 00:34:24 -0600

    escape special make values: fixes #2

--- make_env/__init__.py
+++ make_env/__init__.py
@@ -1,14 +1,30 @@
 import os
 import sys
 
-from dotenv import load_dotenv, find_dotenv
+from dotenv import find_dotenv, dotenv_values
+
+
+def load_make_env():
+    if envfile := find_dotenv(usecwd=True):
+        denv = dotenv_values(envfile)
+        if any((v := os.environ.get(k)) is None or v != denv[k] for k in denv):
+            if not any(
+                map(
+                    lambda x: x in sys.argv,
+                    ["-h", "--help", "--quiet", "--silent", "-s"],
+                )
+            ):
+                print(f"\u001b[1;40;37mLoading .env from {envfile}...\u001b[0m")
+
+        os.environ.update(
+            {
+                k: "" if v is None else v.replace("$", "$$")
+                for k, v in denv.items()
+                if os.environ.get(k) != v
+            }
+        )
 
 
 def main():
-    if envfile := find_dotenv(usecwd=True):
-        if not any(
-            map(lambda x: x in sys.argv, ["-h", "--help", "--quiet", "--silent", "-s"])
-        ):
-            print(f"\u001b[1;40;37mLoading .env from {envfile}...\u001b[0m")
-        load_dotenv(dotenv_path=envfile)
+    load_make_env()
     os.execlp("make", *sys.argv)
---
+++ tests/test_make_env.py
@@ -0,0 +1,34 @@
+import unittest, shutil
+import tempfile
+from make_env import load_make_env
+import os
+
+
+class MakeEnv(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_dir = tempfile.mkdtemp()
+        self.current_dir = os.curdir
+        os.chdir(self.test_dir)
+
+    def tearDown(self) -> None:
+        os.chdir(self.current_dir)
+        shutil.rmtree(self.test_dir)
+
+    def test_dot_env_is_loaded(self) -> None:
+        with open(os.path.join(self.test_dir, ".env"), "w") as f:
+            f.write("TEST_VAR=test_value")
+        load_make_env()
+        self.assertIn("TEST_VAR", os.environ)
+        self.assertEqual(os.environ["TEST_VAR"], "test_value")
+
+    def test_empty_value_not_ignored(self) -> None:
+        with open(os.path.join(self.test_dir, ".env"), "w") as f:
+            f.write("TEST_VAR=")
+        load_make_env()
+        self.assertEqual(os.environ["TEST_VAR"], "")
+
+    def test_special_value_escaped(self) -> None:
+        with open(os.path.join(self.test_dir, ".env"), "w") as f:
+            f.write("TEST_VAR='4dabfs#a%d73w$Z2TBN4!nYD4Y$TW'")
+        load_make_env()
+        self.assertEqual(os.environ["TEST_VAR"], "4dabfs#a%d73w$$Z2TBN4!nYD4Y$$TW")
